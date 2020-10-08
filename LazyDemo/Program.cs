ebb816a775d4e5279fa101300d7e77e8de16f736
Atul Sharma <sharma.atul.kr@gmail.com>
2020-10-08 00:54:48 -0500

    Initial Draft

---
+++ LazyDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LazyDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                var x = DataCache.Names;
+                
+            }
+           // Console.WriteLine("Counter Value =" + DataCache.Counter);
+            Console.ReadLine();
+        }
+    }
+    public sealed class DataCache
+    {
+        private static Lazy<DataCache> local = new Lazy<DataCache>(GetData);
+
+        private List<string> _names = null;
+       
+        public static List<string> Names
+        {
+            get
+            {
+                return local.Value._names;
+            }
+        }
+        private static DataCache GetData()
+        {
+            DataCache cache = new DataCache();
+            cache._names = Employees.GetNames();
+            Console.WriteLine("GetData Method Called");
+            return cache;
+        }
+
+    }
+
+    public class Employees
+    {
+        public static List<string> GetNames()
+        {
+            System.Threading.Thread.Sleep(2000);
+            return new List<string>()
+            {"Atul", "Ramesh", "Mahesh", "Devesh", "Ajay" };
+        }
+    }
+}
